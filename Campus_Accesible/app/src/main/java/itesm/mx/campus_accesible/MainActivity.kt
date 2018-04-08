9b33aa8af75b2bc999b845167936abd0f5240270
Eduardo <ed.trujillo.r@outlook.com>
2018-04-07 19:38:48 -0500

    FrameLayout y funcion para cambiar fragmento creada

--- Campus_Accesible/app/src/main/java/itesm/mx/campus_accesible/MainActivity.kt
+++ Campus_Accesible/app/src/main/java/itesm/mx/campus_accesible/MainActivity.kt
@@ -1,5 +1,6 @@
 package itesm.mx.campus_accesible
 
+import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
@@ -10,25 +11,27 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
+    fun replaceFragment(frag: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, frag).commit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val mapFragment = Fragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, mapFragment).commit()
     }
 }
